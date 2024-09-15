81ac2d932b277597f65f226681ac14714c9e7c25
gk <gyanendra.kumar.che21@itbhu.ac.in>
2024-09-15 16:59:57 +0530

    git commit"check"

--- src/context/globalContext.js
+++ src/context/globalContext.js
@@ -2,11 +2,12 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:5000/api/v1/"; 
+// const BASE_URL = "http://localhost:5000/api/v1/"; 
 // const BASE_URL = "https://backend-expense-tracker-app.onrender.com"; 
 // const BASE_URL = "https://backend-expense-tracker-app.onrender.com/api/v1/"; 
 // const BASE_URL = "https://backend-expense-tracker-app.onrender.com"; 
 // const BASE_URL =process.env.REACT_APP_BASE_URL;
+const BASE_URL="https://backend-expense-tracker-app.onrender.com";
 
 
 const GlobalContext = React.createContext()
